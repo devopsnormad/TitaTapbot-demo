1a23528bbb2476caac28bc9fb22272fbb3e29099
Emmanuel Doji <devopsnormad@gmail.com>
2024-06-21 19:17:50 +0100

    Addition of valid React components to earn, invite and rewards pages

---
+++ pages/earn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const earn = () => {
+  return (
+    <div>earn</div>
+  )
+}
+
+export default earn
---
+++ pages/invite.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const invite = () => {
+  return (
+    <div>invite</div>
+  )
+}
+
+export default invite
